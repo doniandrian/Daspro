0f7b34d0aad357e28aaa912a7b92a477f88cc1f0
BlackArch08 <doniandrian.talenta@gmail.com>
2021-10-01 22:13:01 +0700

    remotetv2

--- Tugas/Minggu3/Remote_tv2.java
+++ Tugas/Minggu3/Remote_tv2.java
@@ -1,5 +1,15 @@
 /**
- * 
+ * idenya menggunakan branching untuk menentukan berapa banyak tombol yang harus ditekan
+ * pertama input bilangan bulat ada 4 buah
+ * jika awal1 lebih besar dari akhir1 maka tombol1 = 100 - awal1+akhir1
+ * jika sebaliknya maka akhir1 - awal1
+ * jika awal2 lebih besar dari akhir2 maka tombol2 = 100 - awal2+akhir2
+ * jika sebaliknya maka akhir2 - awal2
+ * kemudian jika awal3 > akhir3 maka tombo3 = tombol3 = 100 - awal3 + akhir3
+ * jika sebaliknya maka tombol3 = akhir3 -awal3;
+ * output tombol1,tombol2,tombol3
+ * @author Doni Andrian
+ * @version 1 oktober 2021
  */
 
 import java.util.Scanner;
@@ -12,10 +22,30 @@ public class Remote_tv2 {
         int akhir2 = input.nextInt();
         int awal3 = input.nextInt();
         int akhir3 = input.nextInt();
+        int tombol1 = 0; //deklarasi tombol1
+        int tombol2 = 0; //deklarasi tombol2
+        int tombol3 = 0; //deklarasi tombol3
 
-        int tombol1 = Math.abs(akhir1 - awal1);
-        int tombol2 = 100 + (awal2 - akhir2);
-        int tombol3 = 100 + awal3 + akhir3;
+        if (awal1>akhir1){
+            tombol1 = 100 - awal1 + akhir1; //hitung jumlah pemencetan tombol1
+
+        }else{
+            tombol1 = akhir1 - awal1;
+        }
+        if (awal2>akhir2){
+            tombol2 = 100 - awal2 + akhir2; //hitung jumlah pemencetan tombol2
+
+        }else{
+            tombol2 = akhir2 - awal2; //jika akhir2>awal2
+
+        }
+        if (awal3>akhir3){
+            tombol3 = 100 - awal3 + akhir3; //menghitung jumlah pemencetan tombol3 jika awal3>akhir3
+
+        }else{
+            tombol3 = akhir3 - awal3;//jika akhir3>awal3
+        }
+        
         System.out.println(tombol1);
         System.out.println(tombol2);
         System.out.println(tombol3);
