fdcba66860ee711bb82d540e14f20f50a967aff3
BLACKARCH MICROSOFT <doniandrian.talenta@gmail.com>
2021-10-27 17:11:06 +0700

    lat

---
+++ Latihan/Minggu7/bola.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+public class bola {
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
---
+++ Latihan/Minggu7/coba_tebak.java
@@ -0,0 +1,40 @@
+/**
+ * @author Doni Andrian
+ * @version 27 OKTOBER 2021
+ */
+
+import java.util.Scanner;
+public class coba_tebak {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //input
+
+        int n = input.nextInt();
+        input.nextLine();
+        String kata = input.nextLine();
+        input.close();
+        //deklarasi variabel
+        String kata_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+        //char kata_ganti = kata_list.charAt(n-1); 
+        char underscore = '_';
+        
+   
+        
+        kata = kata.replaceAll(" ", ""); //hilangkan spasi dengan menggunakan method replaceAll()
+        //proses
+        for (int i = 0; i < n; i++) {
+            if (kata.charAt(i)==(underscore)) { 
+
+                kata = kata.replace(kata.charAt(i), kata_list.charAt(n-1)); //ganti underscore dengan huruf ganti
+
+            }
+        }
+        
+        
+        System.out.println(kata);
+
+
+        
+    }
+    
+}
